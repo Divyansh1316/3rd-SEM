c4c9aafcf545bcdb244283657f22698750431267
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2023-10-16 08:59:51 +0530

    Create split_ll.c

---
+++ C/split_ll.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct SNodeType
+{
+    int info;
+    struct SNodeType *next;
+} sn;
+
+sn *head = NULL;
+
+sn *inslast(sn *head)
+{
+    sn *temp;
+    temp = (sn *)malloc(sizeof(sn));
+    if (temp == NULL)
+    {
+        printf("Memory is full");
+    }
+    else if (head == NULL)
+    {
+        head = temp;
+        temp->next = NULL;
+        printf("Enter info : ");
+        scanf("%d", &(temp->info));
+    }
+    else
+    {
+        sn *p1 = head;
+        while (p1->next != NULL)
+        {
+            p1 = p1->next;
+        }
+        temp->next = NULL;
+        p1->next = temp;
+        printf("Enter info : ");
+        scanf("%d", &(temp->info));
+    }
+    return head;
+}
+
+sn *traverse(sn *head)
+{
+    sn *p1 = head;
+    if (head == NULL)
+    {
+        printf("Linked list is empty\n");
+    }
+    else
+    {
+        while (p1 != NULL)
+        {
+            printf("%d ", p1->info);
+            p1 = p1->next;
+        }
+        printf("\n");
+    }
+    return head;
+}
+
+sn *split(sn *head)
+{
+    sn *p = head;
+    sn *p2 = head;
+    int n,c=1;
+    printf("Enter position where u want to split : ");
+    scanf("%d",&n);
+    while (c != n)
+    {
+        p = p->next;
+        c++;
+    }
+    p->next=NULL;
+    p=head;
+    while(p!=NULL)
+    {
+        printf("%d",p->info);
+        p=p->next;
+    }
+    head=p->next;
+    return head;
+}
+
+int main()
+{
+    int ch;
+    do
+    {
+        printf("Enter choice : ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            head = inslast(head);
+            break;
+        case 2:
+            head = traverse(head);
+            break;
+        case 3:
+            head = split(head);
+            break;
+        default:
+            printf("Invalid choice");
+            exit(0);
+        }
+    } while (1);
+}

8f6c8a1b1a7e1fb9f91e228e59a269c735474ae7
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2023-10-22 11:29:54 +0530

    Add files via upload

---
+++ C++/task6_2.cpp
@@ -0,0 +1,52 @@
+/*
+2. Create a class called Time that has separate int member data for hours, minutes and seconds.
+One constructor should initialize this data to 0, and another should initialize it to fixed values.
+A member function should display it in 11:59:59 format. A member function named add()
+should add two objects of type time passed as arguments. A main ( ) program should create two
+initialized values together, leaving the result in the third time variable. Finally it should display
+the value of this third variable.
+*/
+
+#include <iostream>
+using namespace std;
+
+class time
+{
+    int h, min, sec;
+
+public:
+    time()
+    {
+        h = 0;
+        min = 0;
+        sec = 0;
+    }
+    time(int x, int y, int z)
+    {
+        h = x;
+        min = y;
+        sec = z;
+    }
+    void display()
+    {
+        cout << "Time is : " << h << ":" << min << ":" << sec << endl;
+    }
+
+    void add(time &ob1, time &ob2)
+    {
+        h = ob1.h + ob2.h;
+        min = ob1.min + ob2.min;
+        sec = ob1.sec + ob2.sec;
+    }
+};
+int main()
+{
+    time ob1(10, 10, 10);
+    ob1.display();
+    time ob2(20, 20, 20);
+    ob2.display();
+    time res;
+    res.add(ob1, ob2);
+    res.display();
+    return 0;
+}
---
+++ C++/task6_3.cpp
@@ -0,0 +1,59 @@
+/*
+3. Create a Base class that consists of private, protected and public data members and member
+functions. Try using different access modifiers for inheriting Base class to the Derived class
+and create a table that summarizes the above three modes (when derived in public, protected
+and private modes) and shows the access specifier of the members of base class in the Derived
+class.(verify and validate access mechanism and validity modes)
+*/
+
+#include <iostream>
+using namespace std;
+
+class base
+{
+private:
+    int a = 10;
+
+protected:
+    int b = 20;
+
+public:
+    int c = 30;
+};
+
+class derivedprivate : private base
+{
+public:
+    void display()
+    {
+        cout << c << b << endl;
+    }
+};
+
+class derivedprotected : protected base
+{
+public:
+    void display()
+    {
+        cout << c << b << endl;
+    }
+};
+class derivedpublic : public base
+{
+public:
+    void display()
+    {
+        cout << c << b << endl;
+    }
+};
+
+int main()
+{
+    derivedprivate ob1;
+    ob1.display();
+    derivedprotected ob2;
+    ob2.display();
+    derivedpublic ob3;
+    ob3.display();
+    return 0;
+}
